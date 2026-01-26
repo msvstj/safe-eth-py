d1529204f39ce2014ff24d20217603f8ce49a0c6
msvstj <savastijonok@gmail.com>
2026-01-26 13:07:09 +0200

    chore: add test

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2663,6 +2663,14 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0+L2",
         ),  # v1.3.0+L2
     ],
+    EthereumNetwork.TEST: [
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 2572371, "1.4.1"),  # v1.4.1
+        (
+            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+            2572415,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -3874,6 +3882,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 11641791),  # v1.4.1
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 11644889),  # v1.3.0
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 11648187),  # v1.3.0
+    ],
+    EthereumNetwork.TEST: [
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 2572371),  # v1.4.1
     ],
 }
 
